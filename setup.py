8a264927cfa7b33fba16a187c70c3a0f33a85932
Nick.Vogt <nick.vogt@dynatrace.com>
2019-03-01 12:08:18 -0500

    add python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
     packages=["dequindre"],
     classifiers=[
         "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
