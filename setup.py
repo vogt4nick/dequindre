8eb2c6bd6f9b46ef92d2fc0a8425f8e65a2aa291
Nick.Vogt <nick.vogt@dynatrace.com>
2019-03-02 12:35:37 -0500

    Add topic to pypi classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
