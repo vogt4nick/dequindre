0ce67f45bcfd75b5d734df43226a5defd43d1c2d
Nick.Vogt <nick.vogt@dynatrace.com>
2019-03-03 12:43:14 -0500

    Bump version: 0.8.4 → 0.8.5.dev0

--- dequindre/__init__.py
+++ dequindre/__init__.py
@@ -15,7 +15,7 @@ from subprocess import run as subprocess_run
 from subprocess import check_output, CalledProcessError
 
 
-__version__ = '0.8.4'
+__version__ = '0.8.5.dev0'
 
 
 class CyclicGraphError(Exception):
