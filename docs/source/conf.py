72cf4fa9532cb6ec1fcfb2828958ba693164803f
Nick.Vogt <nick.vogt@dynatrace.com>
2019-03-06 19:38:16 -0500

    Bump version: 0.10.0.dev0 → 0.10.0.dev1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Nick Vogt'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.10.0.dev0'
+release = '0.10.0.dev1'
 
 
 # -- General configuration ---------------------------------------------------
